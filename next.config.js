c535abe097a356c82fe4cef81ca417668f6452ec
merakissb <mbustosquiroz@gmail.com>
2024-03-21 05:15:51 -0300

    restore optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
-  output: 'export'
+  images: {
+    domains: ["localhost", "metalmaf.cl"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
