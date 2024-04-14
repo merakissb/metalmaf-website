1f47f1cf71b601e84b1bfbf3b2333e74246fd5bf
merakissb <mbustosquiroz@gmail.com>
2024-04-14 12:47:52 -0400

    add paraph in features

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -9,9 +9,8 @@ const Features = () => {
         <div className="container">
           <SectionTitle
             title="Servicios"
-            // paragraph="Nuestro objetivo principal es ayudar a nuestros clientes a desarrollar sus proyectos de forma rápida y profesional, manteniendo un estándar de calidad alta en la manufactura.
-            // Contamos con maquinarias de precisión y un equipo de trabajo especializado en metalurgia, pintura y desarrollos productivos."
-            paragraph=""
+            paragraph="Nuestro objetivo principal es ayudar a nuestros clientes a desarrollar sus proyectos de forma rápida y profesional, manteniendo un estándar de calidad alta en la manufactura.
+            Contamos con maquinarias de precisión y un equipo de trabajo especializado en metalurgia, pintura y desarrollos productivos."
             center
           />
 
